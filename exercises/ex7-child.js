30cf5cb8288706474669c606ed3ae2514bc70203
Erle Monfils <erlemonfils@TVA053.wind3.hub>
2022-04-03 20:39:50 +0200

    child processes

--- exercises/ex7-child.js
+++ exercises/ex7-child.js
@@ -2,17 +2,23 @@
 
 // var fetch = require("node-fetch");
 
+const util = require("util");
+const childProc = require("child_process");
 
 // ************************************
 
 const HTTP_PORT = 8039;
 
-
 main().catch(() => 1);
-
 
 // ************************************
 
 async function main() {
-	// TODO
+  let x = 0;
+  for (let i = 0; i < 1000000000; i++) {
+    x += i;
+  }
+
+  // This will be 0 anyway, this is just to show that we can modify the exit code
+  process.exitCode = 0;
 }
--- exercises/ex7.js
+++ exercises/ex7.js
@@ -3,8 +3,7 @@
 "use strict";
 
 var util = require("util");
-// var childProc = require("child_process");
-
+var childProc = require("child_process");
 
 // ************************************
 
@@ -13,12 +12,20 @@ const HTTP_PORT = 8039;
 
 var delay = util.promisify(setTimeout);
 
-
 main().catch(console.error);
-
 
 // ************************************
 
 async function main() {
-	// console.log(`Load testing http://localhost:${HTTP_PORT}...`);
+  // console.log(`Load testing http://localhost:${HTTP_PORT}...`);
+
+  // Start a separate child process: run a file with node
+  let child = childProc.spawn("node", ["ex7-child.js"]);
+
+  // Get notified when child process finishes
+  child.on("exit", (code) => {
+    // You can check exit code implicitly, by running the node file and following up with && <other command line command>.
+    // If second command doesn't execute, there was an exit code of 1
+    console.log("Child finished", code);
+  });
 }
